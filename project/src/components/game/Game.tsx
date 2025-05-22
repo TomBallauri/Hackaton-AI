155b0c04a5c39a821cecd3d20d88e914e69444aa
AdRI3n30 <boreeadrien@gmail.com>
2025-05-22 15:16:26 +0200

    Update

--- project/src/components/game/Game.tsx
+++ project/src/components/game/Game.tsx
@@ -34,7 +34,7 @@ const Game: React.FC = () => {
     return (
       <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-50 to-indigo-100">
         <video
-          src="/public/vidéo/Boy.mp4" // Mets ici le chemin de ta vidéo
+          src="/vidéo/Boy.mp4" 
           autoPlay
           className="rounded-xl shadow-lg max-w-full"
           onEnded={() => {
